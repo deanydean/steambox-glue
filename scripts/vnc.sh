a1ad7975f2b2a22cb7e23143e0f0fd5e43a24d08
Deany Dean <deanydean@oddcyb.org>
2019-10-14 21:02:00 +0100

    Fixed vnc server script

--- scripts/vnc.sh
+++ scripts/vnc.sh
@@ -2,5 +2,4 @@
 # Start a VNC server
 
 echo "Starting VNC server...."
-/usr/bin/x11vnc -xkb -forever \
-    -rfbauth ${SBG_CONFIG_PATH}/vnc_pwd -rfbport 5900 -bg -o ${SBG_CONFIG_PATH}/x11vnc.log &
+/usr/bin/vncserver -rfbauth ${SBG_CONFIG_PATH}/vnc_pwd -rfbport 5900 -autokill &
